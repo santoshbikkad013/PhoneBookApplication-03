4544fbfc8e7cffc9506e59544b43f00512ea5c3b
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-08-23 12:21:44 +0530

    PBA-03:implementation for Save Method.

--- src/main/java/com/example/demo/service/ContactServiceI.java
+++ src/main/java/com/example/demo/service/ContactServiceI.java
@@ -1,5 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Contact;
+
 public interface ContactServiceI {
 
+	boolean  saveContact(Contact contact);
 }
--- src/main/java/com/example/demo/service/ContactServiceImpl.java
+++ src/main/java/com/example/demo/service/ContactServiceImpl.java
@@ -1,8 +1,27 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Contact;
+import com.example.demo.repository.ContactRepository;
 
 @Service
 public class ContactServiceImpl  implements ContactServiceI{
 
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@Override
+	public boolean saveContact(Contact contact) {
+	
+		Contact save = contactRepository.save(contact);
+		
+		if(save !=null) {
+			return true;
+		}
+		else {
+		return false;
+	}
+	}
 }
