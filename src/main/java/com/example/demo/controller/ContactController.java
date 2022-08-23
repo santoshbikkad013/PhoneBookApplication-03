3131ad9c53844e5a5a02ab6c7316b244277f6729
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-08-23 20:25:56 +0530

    PBA-04: get all contact details from database

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +18,34 @@ public class ContactController {
 
 	@Autowired
 	private ContactServiceI contactServiceI;
-	
-	@PostMapping(value="/saveContact" ,consumes = "application/json")
-	public ResponseEntity<String> saveContact(@RequestBody Contact contact){
-	
+
+	@PostMapping(value = "/saveContact", consumes = "application/json")
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact) {
+
 		boolean saveContact = contactServiceI.saveContact(contact);
-		
-		if(saveContact == true) {
-			String msg="Contact Saved Successfully";
-			return new ResponseEntity<String>(msg,HttpStatus.OK);
-		}else {
-			String msg="Contact  not Saved Successfully";
-			return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
+
+		if (saveContact == true) {
+			String msg = "Contact Saved Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.OK);
+		} else {
+			String msg = "Contact  not Saved Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.BAD_REQUEST);
 		}
-		
-	
-		
+
 	}
 	
+@GetMapping(value ="/getAllContact",produces = "application/json")
+	public ResponseEntity<List<Contact>> getAllContact() {
+	    List<Contact> allContact = contactServiceI.getAllContact();
+		
+	    if(allContact !=null) {
+	    	return new ResponseEntity<List<Contact>>(allContact,HttpStatus.OK);
+	    }else {
+	    	String msg="Data not found";
+	    	return new ResponseEntity(msg,HttpStatus.BAD_REQUEST);
+	    }
 	
+
+	}
+
 }
--- src/main/java/com/example/demo/service/ContactServiceI.java
+++ src/main/java/com/example/demo/service/ContactServiceI.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.model.Contact;
 
 public interface ContactServiceI {
 
-	boolean  saveContact(Contact contact);
+	boolean saveContact(Contact contact);
+
+	List<Contact> getAllContact();
 }
